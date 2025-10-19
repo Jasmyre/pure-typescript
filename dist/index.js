2d85c801fb2ae6664669e512ecb337b42229b8ac
Jasmyre <lanuzajasmyreandrei@gmail.com>
2025-10-19 15:11:32 +0800

    Refactor matrix operations and update cSpell words in settings

--- dist/index.js
+++ dist/index.js
@@ -1,30 +1,11 @@
 import { array, det, inv } from "vectorious";
-const matrixA = array([
+const a = array([
     [1, 0, -3],
     [2, -2, 1],
     [0, -1, 3],
 ]);
-const matrixADet = det(matrixA);
-if (matrixADet != 0) {
-    const matrixAInverse = inv(matrixA);
-    console.table(matrixAInverse.toArray());
+const detA = det(a);
+if (detA !== 0) {
+    const aInv = inv(a);
+    console.table(aInv.toArray());
 }
-const a = array([
-    [1, 2, 3],
-    [2, 4, 6],
-    [3, 6, 9],
-]);
-// const a = array([
-// 	[1, 0, 3],
-// 	[2, -2, 1],
-// 	[0, -1, 3],
-// ]);
-const b = array([
-    [-5, 3, -6],
-    [-6, 3, -7],
-    [-2, 1, -2],
-]);
-const c = a.multiply(b);
-console.log(a);
-console.log(b);
-console.table(c.toArray());
--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,14 @@
 import { array, det, multiply, inv } from "vectorious";
 
-const matrixA = array([
+const a = array([
 	[1, 0, -3],
 	[2, -2, 1],
 	[0, -1, 3],
 ]);
 
-const matrixADet = det(matrixA);
+const detA = det(a);
 
-if (matrixADet != 0) {
-	const matrixAInverse = inv(matrixA);
-	console.table(matrixAInverse.toArray());
+if (detA !== 0) {
+	const aInv = inv(a);
+	console.table(aInv.toArray());
 }
-
-const a = array([
-	[1, 2, 3],
-	[2, 4, 6],
-	[3, 6, 9],
-]);
-// const a = array([
-// 	[1, 0, 3],
-// 	[2, -2, 1],
-// 	[0, -1, 3],
-// ]);
-
-const b = array([
-	[-5, 3, -6],
-	[-6, 3, -7],
-	[-2, 1, -2],
-]);
-
-const c = a.multiply(b);
-
-console.log(a);
-console.log(b);
-console.table(c.toArray());
